10fe06e2def25896e4c50c8c4e757d610cb87449
Pedro Jorquera <pjorquera@okode.com>
2018-08-22 15:27:06 +0200

    Refactor

--- src/controllers/welcome.controller.ts
+++ src/controllers/welcome.controller.ts
@@ -1,24 +1,12 @@
-// Import only what we need from express
 import { Router, Request, Response } from 'express';
 
-// Assign router to the express.Router() instance
 const router = Router();
 
-// The / here corresponds to the route that the WelcomeController
-// is mounted on in the server.ts file.
-// In this case it's /welcome
-router.get('/', (req: Request, res: Response) => {
-    // Reply with a hello world when no name param is provided
-    res.send('Hello, World!');
+router.post('/', (req: Request, res: Response) => {
+  console.log(JSON.stringify(req.body));
+  res.header('Content-Type', 'application/json');
+  let response = { fulfillmentText: 'Hello from Okode Quote Day' };
+  res.send(response);
 });
 
-router.get('/:name', (req: Request, res: Response) => {
-    // Extract the name from the request parameters
-    let { name } = req.params;
-
-    // Greet the given name
-    res.send(`Hello, ${name}`);
-});
-
-// Export the express.Router() instance to be used by server.ts
 export const WelcomeController = router;
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,13 @@
-import * as express from 'express';
-
-// Import WelcomeController from controllers entry point
 import { WelcomeController } from './controllers';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
-// Create a new express application instance
 const app: express.Application = express.default();
+const port = 3000;
 
-// The port the express app will listen on
-const port = process.env.PORT || 3000;
-
-// Mount the WelcomeController at the /welcome route
+app.use(bodyParser.json());
 app.use('/welcome', WelcomeController);
 
-// Serve the application at the given port
 app.listen(port, () => {
-    // Success callback
-    console.log(`Listening at http://localhost:${port}/`);
+  console.log(`Listening at http://localhost:${port}/`);
 });
