89e4bd6cf562f0ff78a5d151a07d3a5322590096
Pedro Jorquera <pjorquera@okode.com>
2018-08-23 21:14:17 +0200

    Added support for parsing parameters

--- src/controllers/assistant.controller.ts
+++ src/controllers/assistant.controller.ts
@@ -1,12 +1,13 @@
 import { Router, Request, Response } from 'express';
-import { dialogflow } from 'actions-on-google';
+import { dialogflow, Parameters } from 'actions-on-google';
 
-const app = dialogflow({ debug: true });
+const app = dialogflow();
 const router = Router();
 router.use(app);
 
 app.intent('favorite color', (conv, params) => {
-  conv.close('Thank you very much for your response');
+  const color = params['color']! as string;
+  conv.close(`Your number is ${color.length}`);
 });
 
 export const AssistantController = router;
