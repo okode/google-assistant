7b6b79a978f13ff123c2a8e9dc375f3ce989a804
Pedro Jorquera <pjorquera@okode.com>
2018-08-23 13:17:27 +0200

    Added google-on-actions SDK

--- src/controllers/assistant.controller.ts
+++ src/controllers/assistant.controller.ts
@@ -1,13 +1,12 @@
 import { Router, Request, Response } from 'express';
+import { dialogflow } from 'actions-on-google';
 
+const app = dialogflow({ debug: true });
 const router = Router();
+router.use(app);
 
-let counter = 0;
-
-router.post('/', (req: Request, res: Response) => {
-  res.header('Content-Type', 'application/json');
-  let response = { fulfillmentText: `Hola desde Okode, mi contador es ${counter++}` };
-  res.send(response);
+app.intent('favorite color', (conv, params) => {
+  conv.close('Thank you very much for your response');
 });
 
 export const AssistantController = router;
