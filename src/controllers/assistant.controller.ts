8abcbf325570f17b4f6a9e38da965eaac43aaf3b
Pedro Jorquera <pjorquera@okode.com>
2018-08-24 09:37:16 +0200

    Added Docker descriptor

--- src/controllers/assistant.controller.ts
+++ src/controllers/assistant.controller.ts
@@ -1,13 +1,35 @@
-import { Router, Request, Response } from 'express';
-import { dialogflow, Parameters } from 'actions-on-google';
+import { Router } from 'express';
+import { dialogflow, Permission } from 'actions-on-google';
 
 const app = dialogflow();
 const router = Router();
 router.use(app);
 
-app.intent('color favorito', (conv, params) => {
-  const color = params['color']! as string;
-  conv.close(`Tu número es ${color.length}`);
+app.intent('Default Welcome Intent', (conv, _) => {
+  conv.ask(new Permission({
+    context: 'Hola, para poder saber mejor de ti',
+    permissions: 'NAME'
+  }));
+});
+
+app.intent('actions_intent_PERMISSION', (conv, _, permissionGranted) => {
+  if (!permissionGranted) {
+    conv.ask('Vale, no te preocupes. ¿Qué tratamiento quieres consultar?');
+  } else {
+    const data = conv.data as { userName?: string, [k: string]: any };
+    data.userName = conv.user.name.display;
+    conv.ask(`Gracias, ${data.userName}. ¿Qué tratamiento quieres consultar?`);
+  }
+});
+
+app.intent('medicina', (conv, params) => {
+  const tratamiento = params['tratamiento']! as string;
+  const data = conv.data as { userName?: string, [k: string]: any };
+  if (data.userName) {
+    conv.close(`${data.userName}, el tratamiento tiene ${tratamiento.length} caracteres`);
+  } else {
+    conv.close(`El tratamiento ${tratamiento} tiene ${tratamiento.length} caracteres`);
+  }
 });
 
 export const AssistantController = router;
